392f06f85ef06b35828cdbed23f7b1845929e4ad
Du <dounra@duonra.me>
2022-09-07 22:00:44 +0200

    sfa

--- Csharp_a-mazing/Tile.cs
+++ Csharp_a-mazing/Tile.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public static class Tile
+public struct Tile
 {
-    public enum TileType 
-    {
-        WALL,
 
+
+    public byte Value { private set; get; }
+
+    public readonly TileType Type;
+
+    public Tile(TileType t = TileType.COLOR, byte val = 0)
+    {
+        this.Type = t;
+        this.Value = val;
+    }
+
+    public enum TileType : byte
+    {
+        BOMB = (byte) 'B',
+        COLOR,
+        TARGET,
+        BONUS,
+        OBSTAKEL = (byte) 'O',
+        DRAAI = (byte) 'R',
+        START,
 
     }
 
